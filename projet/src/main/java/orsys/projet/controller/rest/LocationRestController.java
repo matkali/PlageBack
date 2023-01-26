6efb76e42462febb2b5e306567d40515ab9f1fc6
Augustin Moinat <augustin.moinat@gmail.com>
2023-01-26 16:29:47 +0100

    Premiere méthode get

--- projet/src/main/java/orsys/projet/controller/rest/LocationRestController.java
+++ projet/src/main/java/orsys/projet/controller/rest/LocationRestController.java
@@ -1,5 +1,25 @@
 package orsys.projet.controller.rest;
 
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import orsys.projet.business.Location;
+import orsys.projet.service.LocationService;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
 public class LocationRestController {
 
+	private final LocationService locationService;
+	
+	@GetMapping(value="locations", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Location> getJours() {
+		return locationService.recupererLocation();
+	}
 }
