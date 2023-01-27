990924641cce0fa6143bb9ee295c0c1ddfa52e1a
Mathieu Couchaud <mathieucouchaud@gmail.com>
2023-01-27 09:37:55 +0100

    Update ProjetController.java

--- projet/src/main/java/orsys/projet/controller/ProjetController.java
+++ projet/src/main/java/orsys/projet/controller/ProjetController.java
@@ -4,6 +4,8 @@ import static org.hamcrest.CoreMatchers.nullValue;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,29 +27,33 @@ import orsys.projet.service.UtilisateurService;
 @AllArgsConstructor
 public class ProjetController {
 
-//	private final FileService fileService;
-//	private final LienDeParenteService lienDeParenteService;
-//	private final LocationService locationService;
-//	private final ParasolService parasolService;
-//	private final PaysService paysService;
-//	private final StatutService statutService;
-//	private final UtilisateurService utilisateurService;
-//	
-//	@RequestMapping(value = {"/index", "/"})
-//	public ModelAndView accueil() {
-//		System.out.println(new Date() + " dans l'acceuil");
-//		ModelAndView mav = new ModelAndView();
-//		mav.setViewName("index");
-//		return mav;
-//	}
-//	
-//	@PostMapping("/connexion")
-//	public ModelAndView connexion(@RequestParam("EMAIL") String email, @RequestParam("MOT_DE_PASSE") String motDePasse) {
-//		Utilisateur utilisateur = utilisateurService.recupererUtilisateur(email, motDePasse);
-//		if(utilisateur==null) {
-////			ModelAndView
-//			
-//		}
-//		return null;
-//	}
+	private final FileService fileService;
+	private final LienDeParenteService lienDeParenteService;
+	private final LocationService locationService;
+	private final ParasolService parasolService;
+	private final PaysService paysService;
+	private final StatutService statutService;
+	private final UtilisateurService utilisateurService;
+	private final HttpSession httpSession;
+	
+	@RequestMapping(value = {"/index", "/"})
+	public ModelAndView accueil() {
+		System.out.println(new Date() + " dans l'acceuil");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	@PostMapping("/connexion")
+	public ModelAndView connexion(@RequestParam("EMAIL") String email, @RequestParam("MOT_DE_PASSE") String motDePasse) {
+		Utilisateur utilisateur = utilisateurService.recupererUtilisateur(email, motDePasse);
+		if(utilisateur==null) {
+			ModelAndView mav = accueil();
+			mav.addObject("Notification", "Mot de passe et/ou email incorrect");
+			return mav;
+		} else {
+			httpSession.setAttribute("utilisateurs", utilisateur);
+			return mav;
+		}
+	}
 }
