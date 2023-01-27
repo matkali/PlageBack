3fc4e2619bc5a002d101e1798482c86ba3ce50cd
Augustin Moinat <augustin.moinat@gmail.com>
2023-01-27 17:19:48 +0100

    Communication API
    Ajout de l'annotation CrossOrigin

--- projet/src/main/java/orsys/projet/controller/rest/LocationRestController.java
+++ projet/src/main/java/orsys/projet/controller/rest/LocationRestController.java
@@ -2,6 +2,7 @@ package orsys.projet.controller.rest;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import orsys.projet.service.StatutService;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200/")
 public class LocationRestController {
 
 	private final LocationService locationService;
@@ -36,6 +38,7 @@ public class LocationRestController {
 	
 	@PostMapping(value="location")
 	public Location getLocation(@RequestParam("ID") Long id) {
+		//TODO vérifier la session
 		return locationService.recupererLocationParId(id);
 	}
 	
