fd6226bddfd46bc7d1e9e54a1569af5d73cdfb00
Mathieu Couchaud <mathieucouchaud@gmail.com>
2023-01-31 14:47:00 +0100

    api creationLocation

--- projet/src/main/java/orsys/projet/controller/rest/LocationRestController.java
+++ projet/src/main/java/orsys/projet/controller/rest/LocationRestController.java
@@ -1,16 +1,14 @@
 package orsys.projet.controller.rest;
 
-import static org.hamcrest.CoreMatchers.nullValue;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.validator.constraints.Range;
-import org.springframework.boot.autoconfigure.security.saml2.Saml2RelyingPartyAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +27,6 @@ import orsys.projet.dto.LocationDtoEx;
 import orsys.projet.dto.ParasolDto;
 import orsys.projet.mapper.LocationMapper;
 import orsys.projet.mapper.impl.LocationMapperImpl;
-import orsys.projet.service.ConcessionnaireService;
 import orsys.projet.service.FileService;
 import orsys.projet.service.LocationService;
 import orsys.projet.service.ParasolService;
@@ -39,7 +36,8 @@ import orsys.projet.service.UtilisateurService;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200/")
+@Validated
+@CrossOrigin(origins = "http://localhost:4200")
 public class LocationRestController {
 
 	private final LocationService locationService;
@@ -72,7 +70,7 @@ public class LocationRestController {
 		Statut statut = statutService.recupererStatutsParDebutNom("En").get(0);
 		Concessionnaire concessionnaire = utilisateurService.recupererConcessionnaires().get(0);
 		Locataire locataire = (Locataire) utilisateurService.recupererUtilisateurParEmail(locationDto.getLocataire().getEmail());
-		List<ParasolDto> parasolsDto =locationDto.getParasols();
+		List<ParasolDto> parasolsDto = locationDto.getParasols();
 		List<Parasol> parasols = new ArrayList<>();
 		for(ParasolDto parasol : parasolsDto) {
 			Parasol para = parasolService.recupererParasolParNumEtFile((byte) -1, fileService.recupererFile((byte) parasol.getNumFile()));
